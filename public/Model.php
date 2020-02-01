c57f4ad1bc429570e5ece25b03bf3faea27c8d4f
01Andre <dd.dupin@gmail.com>
2020-02-01 23:27:35 +0100

    prenom et nom ok

--- public/Model.php
+++ public/Model.php
@@ -15,7 +15,9 @@ if ($_POST){
     $methodToExecute = "getQueryWith".namesOfInputFilled($_POST)."Filled";
     $query = $methodToExecute($_POST);
     $statement = $pdo->query($query);
+    var_dump($query);
     $agents = $statement->fetchAll();
+    var_dump($agents);
 
 }
 
@@ -37,14 +39,19 @@ function getQueryWithNniFilled($post)
 
 function getQueryWithNniNomFilled($post)
 {
-    return 'SELECT * FROM agents WHERE nni = ' .$post['nni'] . ' AND ' . $post['nom'];
+    return "SELECT * FROM agents WHERE nni = '" .$post['nni'] . "' AND '" . $post['nom']."'";
 }
 
 function getQueryWithNomFilled($post)
 {
-    return 'SELECT * FROM agents WHERE nom = ' .$post['nom'];
+    return "SELECT nni, nom, prenom FROM agents WHERE nom = '" .$post['nom']."'";
 }
 
 function getQueryWithPrenomFilled($post){
-    return 'SELECT * FROM agents WHERE prenom = ' .$post['prenom'];
+    return "SELECT nni, nom, prenom FROM agents WHERE prenom = '" .$post['prenom']."'";
+}
+
+function getQueryWithNomPrenomFilled($post)
+{
+    return "SELECT nni, nom, prenom FROM agents WHERE prenom = '" .$post['prenom']."' AND nom = '". $post['nom']."'";
 }
