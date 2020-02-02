39346a3fde5bbac38cbe63141aaa4fed23c7b064
01Andre <dd.dupin@gmail.com>
2020-02-02 01:27:27 +0100

    nni seul fonctionnel... mais ça marche pas mal !

--- public/index.php
+++ public/index.php
@@ -5,75 +5,76 @@ require "Model.php";
 <!doctype html>
 <html lang="en">
 <head><?php var_dump($_POST['nom']) ?>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+  <title id="documentTitle" data-length= <?= count($agents) ?>>Hello, world!</title>
 </head>
 <body>
 <form method="POST" action="">
-    <div class="row justify-content-around">
-        <div class="col-4">
-          <input  name="nni" class="custom-select w-75" type="text" list="nnis" placeholder="054859" />
-          <datalist id="nnis">
-                <?php foreach ($agents as $agent => $i) { ?>
-                    <option value="<?php echo $i['nni'] ?>" selected><?php echo $i['nni']?></option>
-                <?php } ?>
+  <div class="row justify-content-around">
+    <div class="col-4">
+      <input id="nni" name="nni" class="custom-select w-75" type="text" list="nnis" placeholder="054859"/>
+      <datalist id="nnis">
+          <?php foreach ($agents as $agent => $i) { ?>
+            <option value="<?php echo $i['nni'] ?>" selected><?php echo $i['nni'] ?></option>
+          <?php } ?>
 
-          </datalist>
-        </div>
-        <div class="col-4">
-            <input  name="nom" class="custom-select w-75" type="text" list="names" placeholder="Dupont">
-            <datalist id="names">
-                <?php if(!$nni) : ?>
-                    <?php foreach ($agents as $agent => $i) { ?>
-                        <option value="<?php echo $i['nom'] ?>" selected><?php echo $i['nom']?></option>
-                    <?php } ?>
-                <?php else: ?>
-                    <?php foreach ($nni as $agent => $i) { ?>
-                        <option value="<?php echo $i['nom'] ?>" selected><?php echo $i['nom']?></option>
-                    <?php } ?>
-                <?php endif ?>
-            </datalist>
-        </div>
-        <div class="col-4">
-          <input  name="prenom" class="custom-select w-75" type="text" list="firstnames" placeholder="Robert">
-          <datalist id="firstnames">
-
-                <option value="">Tous les prenoms</option>
-                <?php foreach ($agents as $agent => $i) { ?>
-                    <option value="<?php echo $i['prenom'] ?>" selected><?php echo $i['prenom']?></option>
-                <?php } ?>
-
-          </datalist>
-        </div>
+      </datalist>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <div class="col-4">
+      <input id="lastname" name="nom" class="custom-select w-75" type="text" list="names" placeholder="Dupont">
+      <datalist id="names">
+          <?php if (!$nni) : ?>
+              <?php foreach ($agents as $agent => $i) { ?>
+              <option value="<?php echo $i['nom'] ?>" selected><?php echo $i['nom'] ?></option>
+              <?php } ?>
+          <?php else: ?>
+              <?php foreach ($nni as $agent => $i) { ?>
+              <option value="<?php echo $i['nom'] ?>" selected><?php echo $i['nom'] ?></option>
+              <?php } ?>
+          <?php endif ?>
+      </datalist>
+    </div>
+    <div class="col-4">
+      <input id="firstname" name="prenom" class="custom-select w-75" type="text" list="firstnames" placeholder="Robert">
+      <datalist id="firstnames">
+
+        <option value="">Tous les prenoms</option>
+          <?php foreach ($agents as $agent => $i) { ?>
+            <option value="<?php echo $i['prenom'] ?>" selected><?php echo $i['prenom'] ?></option>
+          <?php } ?>
+
+      </datalist>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 <table class="table table-dark">
-    <thead>
-    <tr>
-        <th scope="col">nni</th>
-        <th scope="col">nom</th>
-        <th scope="col">prénom</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($agents as $agent => $i) { ?>
-<!--        --><?php //var_dump($nni[0]['nni']) ?>
+  <thead>
+  <tr>
+    <th scope="col">nni</th>
+    <th scope="col">nom</th>
+    <th scope="col">prénom</th>
+  </tr>
+  </thead>
+  <tbody>
 
-    <tr>
-            <td><?php echo $i['nni'] ?></td>
-            <td><?php echo $i['nom'] ?></td>
-            <td><?php echo $i['prenom'] ?></td>
-        </tr>
-    <?php } ?>
-    </tbody>
+  <?php foreach ($agents as $agent => $i) { ?>
+      <?php // var_dump($agent) ?>
+
+    <tr id="tr<?= $agent ?>" class="">
+      <td><?php echo $i['nni'] ?></td>
+      <td><?php echo $i['nom'] ?></td>
+      <td><?php echo $i['prenom'] ?></td>
+    </tr>
+  <?php } ?>
+  </tbody>
 </table>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -86,5 +87,36 @@ require "Model.php";
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
         crossorigin="anonymous"></script>
+
+<script type="text/javascript">
+    document.onload = initialize();
+
+    function initialize() {
+
+
+        let nniInput = document.getElementById('nni');
+        let lastnameInput = document.getElementById('lastname');
+        let firsnameInput = document.getElementById('firstname');
+        nniInput.addEventListener("keyup", function () {
+            checkCompatibleNni(nniInput)
+        });
+    }
+
+    function checkCompatibleNni(nniInput) {
+        let title = document.getElementById('documentTitle');
+        let arrayLength = title.dataset.length;
+
+        for (let i = 0; i < arrayLength; i++) {
+            let container = document.getElementById('tr' + i);
+            if (!(container.firstElementChild.innerHTML.match(nniInput.value))) {
+                container.classList.add("d-none");
+            } else {
+                container.classList.remove('d-none');
+            }
+
+        }
+
+    }
+</script>
 </body>
 </html>
