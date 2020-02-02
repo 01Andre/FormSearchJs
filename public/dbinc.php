bd675b96ed21d89d628aee4d043e2450bd0c28fe
01Andre <dd.dupin@gmail.com>
2020-02-03 00:28:19 +0100

    dbinc

--- public/dbinc.php
+++
@@ -1,4 +0,0 @@
-<?php
-$password = "";/*NOTER VOTRE MOT DE PASSE DE BASE DE DONNEES ICI ENTRE LES GUILLEMETS*/
-$user = "";/*NOTER VOTRE NOM D'UTILISATEUR DE BASE DE DONNES ICI ENTRE LES GUILLEMETS*/
-$pdo = new PDO('mysql:host=localhost;dbname=testsearch', $user, $password);
