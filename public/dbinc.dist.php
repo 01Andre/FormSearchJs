b8cbdf118b981a52d06cc51afa39875d212c541e
01Andre <dd.dupin@gmail.com>
2020-02-03 00:37:52 +0100

    réglement souci gitignore dbinc

---
+++ public/dbinc.dist.php
@@ -0,0 +1,6 @@
+<?php
+//COPIER CE FICHIER AU MEME ENDROIT, ET LE RENOMMER dbinc.php
+
+$password = "";/*NOTER VOTRE MOT DE PASSE DE BASE DE DONNEES ICI ENTRE LES GUILLEMETS*/
+$user = "";/*NOTER VOTRE NOM D'UTILISATEUR DE BASE DE DONNES ICI ENTRE LES GUILLEMETS*/
+$pdo = new PDO('mysql:host=localhost;dbname=testsearch', $user, $password);
